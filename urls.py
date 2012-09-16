0a0d3df9610dc6069b248d4a62be81ed9d55120c
Nikos Roussos <nikos@roussos.cc>
2012-09-16 16:36:42 +0300

    robots.txt added

--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
+from django.views.generic.simple import direct_to_template
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -11,7 +12,8 @@ urlpatterns = patterns('',
     #admin
     #url(r'^admin/', include(admin.site.urls)),
     
-    url(r'^robots\.txt$', lambda r: HttpResponse("User-agent: *\nDisallow: /", mimetype="text/plain"))
+    #url(r'^robots\.txt$', lambda r: HttpResponse("User-agent: *\nDisallow: /", mimetype="text/plain"))
+    (r'^robots\.txt$', direct_to_template, {'template': 'robots.txt', 'mimetype': 'text/plain'}),
 )
 
 
