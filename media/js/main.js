2bb59b4877702e96adb9d238cc5b1029b46dc8d9
Nikos Roussos <nikos@roussos.cc>
2012-10-20 12:33:42 +0300

    added hide menu button

--- media/js/main.js
+++ media/js/main.js
@@ -11,19 +11,23 @@ $(document).ajaxStart(function() {
 
 $(document).ready(function() {
     $("#loading").toggle();
-	
+
     $("a#myloc").click(function() {
         myloc();
     });
-    
+
     $("input#query").keypress(function(e) {
         if (e.which == 13) {
             $("a#searchbutton").click();
         }
     });
-    
+
     $("a#searchbutton").click(function() {
         var query = $("input#query").val();
         $("#searchresults").load("/search/"+query+"/");
+    });
+
+    $("a#meta_button").click(function() {
+        $("main").slideToggle();
     });
 });
