3b4fee8f3774a640d515466f2e5b5a286cf979f9
Nikos Roussos <nikos@roussos.cc>
2012-10-20 15:24:56 +0300

    added basic search functionality

--- functions.py
+++ functions.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 
 def get_results(query):
-    import urllib2
-    from BeautifulSoup import BeautifulSoup
-    xml = urllib2.urlopen("http://open.mapquestapi.com/nominatim/v1/search?format=xml&q=%s" % query)
-    soup = BeautifulSoup(xml)
+    import urllib2, json
+    #from BeautifulSoup import BeautifulSoup
+    jsonurl = urllib2.urlopen("http://open.mapquestapi.com/nominatim/v1/search?format=json&q=%s" % query)
+    data = json.load(jsonurl)
+    places = data
+    #soup = BeautifulSoup(xml)
     #places = {}
-    places = soup.searchresults.findAll('place')
+    #places = soup.searchresults.findAll('place')
     #for place in soup.findALL('place'):
     #    places = dict(place.attrs)
     #places = soup.findALL('place')
