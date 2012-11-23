1811eda52b7a826124147ec6f389f670f4473aa6
comzeradd <nikos@roussos.cc>
2012-11-23 15:31:15 +0200

    add manifest url

--- urls.py
+++ urls.py
@@ -16,6 +16,7 @@ urlpatterns = patterns('',
     #url(r'^admin/', include(admin.site.urls)),
 
     (r'^robots\.txt$', direct_to_template, {'template': 'robots.txt', 'mimetype': 'text/plain'}),
+    (r'^manifest\.webapp$', direct_to_template, {'template': 'manifest.webapp', 'mimetype': 'text/plain'}),
 )
 
 
